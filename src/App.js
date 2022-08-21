3e1628a623848043a5c7ebb708b68bbb34d1540c
Harold J. A. Chelala <chelala@gmail.com>
2022-08-21 04:38:18 -0500

    version con increment

--- src/App.js
+++ src/App.js
@@ -1,8 +1,53 @@
 import React from 'react';
+import {useEffect, useState} from 'react';
+import "./index.css";
 
-function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+export default function App() {
+  const [value, setValue] = useState();
+  const [isInit, setInit] = useState();
+
+  useEffect(() => {
+    fetch("https://thefunkiestapiv999.azurewebsites.net/api/getscore?name=Harold", {
+      method: "GET"
+    })
+    .then((res) => res.json())
+    .then((res) => {
+      setValue(res.data)
+      setInit(true);
+    })
+  }, []);
+
+  useEffect(() => {
+    if (isInit === true) {
+      fetch("https://thefunkiestapiv999.azurewebsites.net/api/updatescore?name=Harold", {
+        method: "GET"
+      })
+      .then((res) => res.json())
+      .then((res) => {
+        setValue(res.data)
+      });        
+    }
+  }, [value]);
+
+  const increment = () => {
+    console.log ("Increment Click");
+    setValue(value + 1);
+  };
+
+  const decrement = () => {
+    console.log ("Decrement Click");
+    //setValue(value - 1);
+  };
+
+  const Button = (props) => {
+    return <button onClick={props.handleClick}>{props.children}</button>
+  }
+
+  return (
+    <div className='App'>    
+      <h2>Hello! value {value}</h2>
+      <Button handleClick={decrement}>Decrement</Button>
+      <Button handleClick={increment}>Increment</Button>
+    </div>
+  )
 }
-
-export default App;
